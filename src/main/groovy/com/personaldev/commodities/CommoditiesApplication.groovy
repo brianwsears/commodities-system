3dab5394b9cc3cb15c0e2e557389f05380a22349
Brian Sears <briansears@carfax.com>
2018-06-01 11:03:24 -0500

    Initial Checkin

---
+++ src/main/groovy/com/personaldev/commodities/CommoditiesApplication.groovy
@@ -0,0 +1,12 @@
+package com.personaldev.commodities
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CommoditiesApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run CommoditiesApplication, args
+    }
+}
---
+++ src/test/groovy/com/personaldev/commodities/CommoditiesApplicationTests.groovy
@@ -0,0 +1,16 @@
+package com.personaldev.commodities
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class CommoditiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
