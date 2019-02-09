f01ef9d784f5afa958defa2719e76caaf7a70a1e
Brian Sears <briansears@carfax.com>
2019-02-09 08:02:22 -0600

    Added tests for the CustomerController

--- src/test/groovy/com/personaldev/commodities/controller/CustomerControllerSpec.groovy
+++ src/test/groovy/com/personaldev/commodities/controller/CustomerControllerSpec.groovy
@@ -2,6 +2,8 @@ package com.personaldev.commodities.controller
 
 import com.personaldev.commodities.BaseSpec
 import com.personaldev.commodities.domain.customer.Customer
+import com.personaldev.commodities.domain.customer.CustomerAddress
+import com.personaldev.commodities.domain.customer.CustomerPhone
 import com.personaldev.commodities.service.CustomerService
 
 class CustomerControllerSpec extends BaseSpec {
@@ -14,13 +16,39 @@ class CustomerControllerSpec extends BaseSpec {
         )
     }
 
-//    def "getUser returns a Customer object"() {
-//        given:
-//            controller.customerService.getCustomer(_) >> mockCustomer
-//        when:
-//            def response = controller.customerService.getCustomer("email")
-//        then:
-//            1 * controller.customerService.getCustomer(_)
-//            response instanceof Customer
-//    }
+    def "creatCustomer returns a Customer object"() {
+        given:
+            controller.customerService.createCustomer(_) >> mockCustomer
+        when:
+            def response = controller.createCustomer(mockCustomer)
+        then:
+            response instanceof Customer
+    }
+
+    def "getCustomer returns a Customer object"() {
+        given:
+            controller.customerService.getCustomer(_) >> mockCustomer
+        when:
+            def response = controller.customerService.getCustomer(TEST_EMAIL)
+        then:
+            response instanceof Customer
+    }
+
+    def "getCustomerAddressList returns a list of CustomerAddress objects"() {
+        given:
+            controller.customerService.getCustomerAddressList(_) >> mockCustomerAddressList
+        when:
+            def response = controller.customerService.getCustomerAddressList(TEST_EMAIL)
+        then:
+            response instanceof List<CustomerAddress>
+    }
+
+    def "getCustomerPhoneList returns a list of CustomerPhone objects"() {
+        given:
+            controller.customerService.getCustomerPhoneList(_) >> mockCustomerPhoneList
+        when:
+            def response = controller.getCustomerPhoneList(TEST_EMAIL)
+        then:
+            response instanceof List<CustomerPhone>
+    }
 }
